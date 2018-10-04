d27a55a2d67c96774ee6caea92345dbdd2d8cd52
t00200043 <michael.j.crowleymcdevitt@students.ittralee.ie>
2018-10-04 15:10:28 +0100

    Working on Message.java and MessageDriver.java

--- src/Message.java
+++ src/Message.java
@@ -1,9 +1,43 @@
 public class Message {
 
-    public Message() {
+    private String sender, recipient, emailBody;
 
-        private String sender, recipient, emailBody;
+    public Message(String sender, String recipient, String emailBody) {
 
+        this.sender = sender;
+        this.recipient = recipient;
+        this.emailBody = emailBody;
+    }
 
+    //Set Methods
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public void setEmailBody(String emailBody) {
+        this.emailBody = emailBody;
+    }
+
+    //Get Methods
+
+    public String getSender() {
+        return sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public String getEmailBody() {
+        return emailBody;
+    }
+
+    public String toString() {
+        return "From: " + this.sender + "\nTo: " + this.recipient + "\nEmail: " + this.emailBody;
     }
 }
