577f43f94d9a3fb025d2aaa7933f93ac2ff7fe30
t00200043 <michael.j.crowleymcdevitt@students.ittralee.ie>
2018-10-04 14:32:58 +0100

    Completed Book.java, BookDriver.java and BookDriverUserDefined.java

--- src/BookDriverUserDefined.java
+++ src/BookDriverUserDefined.java
@@ -2,19 +2,33 @@ import javax.swing.*;
 
 public class BookDriverUserDefined {
 
-    Book worstBook = new Book();
-
     public static void main(String[] args) {
+
+        JOptionPane.showMessageDialog(null,"Worst Book\n-------------------\n" + worstBookDetails() + "\n\nFavourite Book\n-------------------\n" + FavBookDetails());
     }
 
-    public String worstBookDetails();
+    public static String worstBookDetails()
     {
+        Book worstBook = new Book();
+
         worstBook.setTitle(JOptionPane.showInputDialog("Enter your least favorite book's title: "));
         worstBook.setIsbn(JOptionPane.showInputDialog("Enter your least favorite book's ISBN: "));
-        String pageNumString = JOptionPane.showInputDialog("Enter your least favorite book's page number: ");
-        String priceString = JOptionPane.showInputDialog("Enter your least favorite book's price: ");
+        worstBook.setPageNum(Integer.parseInt(JOptionPane.showInputDialog("Enter your least favorite book's page number: ")));
+        worstBook.setPrice(Double.parseDouble(JOptionPane.showInputDialog("Enter your least favorite book's price: ")));
 
         return worstBook.toString();
+    }
+
+    public static String FavBookDetails()
+    {
+        Book favBook = new Book();
+
+        favBook.setTitle(JOptionPane.showInputDialog("Enter your favorite book's title: "));
+        favBook.setIsbn(JOptionPane.showInputDialog("Enter your favorite book's ISBN: "));
+        favBook.setPageNum(Integer.parseInt(JOptionPane.showInputDialog("Enter your favorite book's page number: ")));
+        favBook.setPrice(Double.parseDouble(JOptionPane.showInputDialog("Enter your favorite book's price: ")));
+
+        return favBook.toString();
     }
 
 }
