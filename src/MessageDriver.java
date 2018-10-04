a0988039dbc990c1c172f47fa33656341bb3f5a6
t00200043 <michael.j.crowleymcdevitt@students.ittralee.ie>
2018-10-04 15:54:44 +0100

    Finished Message.java and MessageDriver.java

--- src/MessageDriver.java
+++ src/MessageDriver.java
@@ -7,11 +7,9 @@ public class MessageDriver {
         m.append(append());
         JOptionPane.showMessageDialog(null,m.toString());
 
-
     }
 
     public static Message messageConstruct() {
-
 
         String sender = JOptionPane.showInputDialog("Enter the sender for email: ");
         String recipient = JOptionPane.showInputDialog("Enter the recipient of this email: ");
