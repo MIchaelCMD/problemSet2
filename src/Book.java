1f20f94fbddb5822677b17aa49f98f96e9606c7c
MichaelCMD <michael.j.crowleymcdevitt@students.ittralee.ie>
2018-10-04 12:03:10 +0100

    added more to Book.java, modified BookDriver.java

--- src/Book.java
+++ src/Book.java
@@ -4,6 +4,38 @@ public class Book {
         private int pageNum;
         private double price;
 
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setPageNum(int pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public int getPageNum() {
+        return pageNum;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
     public Book() {
 
     }
--- src/BookDriver.java
+++ src/BookDriver.java
@@ -1,5 +1,9 @@
 public class BookDriver {
     public static void main(String[] args) {
         Book book1 = new Book();
+
+        book1.setIsbn("122e1rt36y754");
+
+        System.out.print(book1.getIsbn());
     }
 }
