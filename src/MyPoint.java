a575fa7a10d68693fb8d2df2a391ec6e120b5d64
t00200043 <michael.j.crowleymcdevitt@students.ittralee.ie>
2018-10-16 17:00:13 +0100

    trying this MyPoint question

--- src/MyPoint.java
+++ src/MyPoint.java
@@ -2,17 +2,27 @@ public class MyPoint {
 
    double xVal, yVal;
 
-    public MyPoint {
+    public MyPoint() {
         this.xVal = 0.0;
         this.yVal = 0.0;
     }
 
-    public void setxVal(double xVal, double yVal) {
+    public void setValues(double xVal, double yVal) {
         this.xVal = xVal;
         this.yVal = yVal;
     }
 
-    public double distanceFromOrigin( double a, double b) {
+    public void double distanceFromOrigin(double a, double b) {
 
+        double c;
+
+        c = Math.sqrt((a*a) + (b*b));
+
+        return c;
+
+    }
+
+    public String toString() {
+        return "The x value is: " + this.xVal + "\nThe y value is: " + this.yVal;
     }
 }
--- src/MyPointMaker.java
+++ src/MyPointMaker.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class MyPointMaker {
     public static void main(String[] args) {
+
+        MyPoint point1 = new MyPoint();
+        point1.setValues(Double.parseDouble(JOptionPane.showInputDialog("Enter the x value: ")),Double.parseDouble(JOptionPane.showInputDialog("Enter the y value: ")));
+
+        JOptionPane.showMessageDialog(null,"Distance from origin: " + point1.distanceFromOrigin(point1.xVal,point1.yVal));
 
     }
 }
